1f286bd1f9f5d5aa04e9e22f97eaace35706c14b
Kostiantyn Shyrolapov <ks1379@scarletmail.rutgers.edu>
2023-01-16 01:02:36 -0500

    init with task upload

---
+++ src/server/api/routers/tasks.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+
+export const tasksRouter = createTRPCRouter({
+  createTask: publicProcedure
+    .input(z.object({ title: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.task.create({
+        data: {
+          ...input,
+        },
+      });
+    }),
+
+  getTasks: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.task.findMany();
+  }),
+
+  // updateTask: protectedProcedure
+  //   .input(z.object({ id: z.number(), title: z.string() }))
+  //   .query(async ({ ctx, input }) => {
+  //     return ctx.prisma.example.update({
+  //       where: { id: input.id },
+  //       data: {
+  //         title: input.title,
+  //       },
+  //     });
+  //   }
+  // ),
+});
