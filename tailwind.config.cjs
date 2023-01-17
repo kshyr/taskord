2d8f7b63c5d7027f485ca74b387e2cfdf6a4cb71
Kostiantyn Shyrolapov <ks1379@scarletmail.rutgers.edu>
2023-01-16 19:28:50 -0500

    add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["e-Ukraine", "sans-serif"],
+      head: ["e-Ukraine Head", "sans-serif"],
+    },
   },
   plugins: [],
 };
