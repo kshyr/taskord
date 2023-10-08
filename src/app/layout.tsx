997c7f128c7472896981ec4a2431a89d35eb8e35
deko95 <kostya.shirolapov@gmail.com>
2023-10-07 22:20:32 -0400

    put frontend code under src, add graphql query on fe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from "@/src/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import TaskWidget from "@/src/components/TaskWidget";
+import {request, gql} from 'graphql-request'
+
+async function getData() {
+  const res = await request(process.env.API_URL, gql`
+    query {
+      allTasks {
+        id
+        name
+        description
+        status
+        priority
+        createdAt
+        updatedAt
+      }
+    }
+  `);
+  return JSON.stringify(res)
+}
+
+export default async function Home() {
+  const data = await getData();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {data}
+      <TaskWidget />
+    </main>
+  );
+}
