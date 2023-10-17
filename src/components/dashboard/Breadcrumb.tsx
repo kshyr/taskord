b2b322eb0ca67a1d648603121c036c9e73231241
Kostiantyn Shyrolapov <kostya.shirolapov@gmail.com>
2023-10-17 10:05:31 -0400

    fix slug generation, add components.json

--- src/components/dashboard/Breadcrumb.tsx
+++ src/components/dashboard/Breadcrumb.tsx
@@ -1,36 +1,37 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import {usePathname} from "next/navigation";
 import Link from "next/link";
-import { cn } from "@/src/utils/styles.utils";
-import { decodeSlug } from "@/src/utils/general.utils.ts";
+import {cn} from "@/src/utils/styles.utils";
+import {decodeSlug} from "@/src/utils/general.utils.ts";
+import {Fragment} from "react";
 
 export default function Breadcrumb() {
-  const pathname = usePathname();
+	const pathname = usePathname();
 
-  return (
-    <div className="flex h-full gap-2">
-      {pathname
-        .slice(1)
-        .split("/")
-        .map((path, index, paths) => (
-          <>
-            <Link
-              href={`/${paths.slice(0, index + 1).join("/")}`}
-              className={cn(
-                index === paths.length - 1
-                  ? "cursor-default"
-                  : "text-muted-foreground",
-                "font-head text-xl capitalize tracking-wider",
-              )}
-            >
-              {decodeSlug(path)}
-            </Link>
-            {index === paths.length - 1 || (
-              <span className="text-[22px] text-muted-foreground">/</span>
-            )}
-          </>
-        ))}
-    </div>
-  );
+	return (
+		<div className="flex h-full gap-2">
+			{pathname
+			.slice(1)
+			.split("/")
+			.map((path, index, paths) => (
+				<Fragment key={"bread-" + path + index}>
+					<Link
+						href={`/${paths.slice(0, index + 1).join("/")}`}
+						className={cn(
+							index === paths.length - 1
+								? "cursor-default"
+								: "text-muted-foreground",
+							"font-head text-xl capitalize tracking-wider",
+						)}
+					>
+						{decodeSlug(path)}
+					</Link>
+					{index === paths.length - 1 || (
+						<span className="text-[22px] text-muted-foreground">/</span>
+					)}
+				</Fragment>
+			))}
+		</div>
+	);
 }
--- src/utils/general.utils.ts
+++ src/utils/general.utils.ts
@@ -1,10 +1,14 @@
 export function encodeSlug(string: string): string {
-  return string
-    .toLowerCase()
-    .replace(/ /g, "-")
-    .replace(/[^\w-]+/g, "");
+	return string
+	.toLowerCase()
+	.replace(/-/g, "_")
+	.replace(/ /g, "-")
+	.replace(/[^\w_-]+/g, "");
 }
 
 export function decodeSlug(slug: string): string {
-  return slug.replace(/-/g, " ").replace(/(^\w|\s\w)/g, (m) => m.toUpperCase());
+	return slug
+	.replace(/-/g, " ")
+	.replace(/_/g, "-")
+	.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase());
 }
