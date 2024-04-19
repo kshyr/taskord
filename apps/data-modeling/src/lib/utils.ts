4913b4b96f495b0a1416623183112fed5e19778d
Kostiantyn Shyrolapov <kostya.shirolapov@gmail.com>
2024-04-18 23:17:58 -0400

    type entity, override react-flow Node with enum of existing types

--- apps/data-modeling/src/lib/utils.ts
+++ apps/data-modeling/src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import type { Entity, RFNode } from '../types';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const createNodeFromEntity = (entity: Entity): RFNode => {
+  // TODO: Implement the logic to create a node from the entity
+  return {
+    id: entity.name,
+    type: 'position-logger',
+    position: { x: 0, y: 0 },
+    data: entity,
+  };
+};
--- apps/data-modeling/src/types/index.tsx
+++ apps/data-modeling/src/types/index.tsx
@@ -1,8 +1,31 @@
+import type { Node } from 'reactflow';
+
+import { nodeTypes } from '../canvas/nodes';
+
 export type Entity = {
   name: string;
-  fields: {
-    name: string;
-    type: string;
-    required: boolean;
-  }[];
+  fields: Field[];
 };
+
+export type Field = {
+  name: string;
+  type: FieldType;
+  required: boolean;
+};
+
+export const FieldType = {
+  STRING: 'string',
+  NUMBER: 'number',
+  BOOLEAN: 'boolean',
+  DATETIME: 'datetime',
+  ENUM: 'enum',
+  RELATION: 'relation',
+  LIST: 'list',
+} as const;
+export type FieldType = (typeof FieldType)[keyof typeof FieldType];
+
+export type NodeType = keyof typeof nodeTypes;
+
+export interface RFNode extends Node {
+  type: NodeType;
+}
